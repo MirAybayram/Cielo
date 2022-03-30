fa94edb04acdf5fb864116950c5f4ca52e495635
MirAybayam <melikemiraybyrm@gmail.com>
2022-03-30 20:30:20 +0300

    Add project files.

---
+++ Cielo/FrmMenu.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Cielo
+{
+    partial class FrmMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMenu));
+            this.btnRaporlar = new System.Windows.Forms.Button();
+            this.btnCıkıs = new System.Windows.Forms.Button();
+            this.btnRapor = new System.Windows.Forms.Button();
+            this.btnKasaİslemleri = new System.Windows.Forms.Button();
+            this.btnMusteriler = new System.Windows.Forms.Button();
+            this.btnRezervasyon = new System.Windows.Forms.Button();
+            this.btnMasaİslemleri = new System.Windows.Forms.Button();
+            this.btnMutfak = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRaporlar
+            // 
+            this.btnRaporlar.BackColor = System.Drawing.Color.Transparent;
+            this.btnRaporlar.BackgroundImage = global::Cielo.Properties.Resources.Raporlar;
+            this.btnRaporlar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnRaporlar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRaporlar.Location = new System.Drawing.Point(150, 273);
+            this.btnRaporlar.Name = "btnRaporlar";
+            this.btnRaporlar.Size = new System.Drawing.Size(142, 87);
+            this.btnRaporlar.TabIndex = 0;
+            this.btnRaporlar.UseVisualStyleBackColor = false;
+            // 
+            // btnCıkıs
+            // 
+            this.btnCıkıs.BackColor = System.Drawing.Color.Transparent;
+            this.btnCıkıs.BackgroundImage = global::Cielo.Properties.Resources.çıkış;
+            this.btnCıkıs.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnCıkıs.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCıkıs.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCıkıs.Location = new System.Drawing.Point(431, 273);
+            this.btnCıkıs.Name = "btnCıkıs";
+            this.btnCıkıs.Size = new System.Drawing.Size(102, 87);
+            this.btnCıkıs.TabIndex = 0;
+            this.btnCıkıs.UseVisualStyleBackColor = false;
+            this.btnCıkıs.Click += new System.EventHandler(this.btnCıkıs_Click);
+            // 
+            // btnRapor
+            // 
+            this.btnRapor.BackColor = System.Drawing.Color.Transparent;
+            this.btnRapor.BackgroundImage = global::Cielo.Properties.Resources.ayarlar;
+            this.btnRapor.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnRapor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRapor.Location = new System.Drawing.Point(298, 273);
+            this.btnRapor.Name = "btnRapor";
+            this.btnRapor.Size = new System.Drawing.Size(127, 87);
+            this.btnRapor.TabIndex = 0;
+            this.btnRapor.UseVisualStyleBackColor = false;
+            // 
+            // btnKasaİslemleri
+            // 
+            this.btnKasaİslemleri.BackColor = System.Drawing.Color.Transparent;
+            this.btnKasaİslemleri.BackgroundImage = global::Cielo.Properties.Resources.Kasa;
+            this.btnKasaİslemleri.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnKasaİslemleri.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnKasaİslemleri.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnKasaİslemleri.Location = new System.Drawing.Point(257, 165);
+            this.btnKasaİslemleri.Name = "btnKasaİslemleri";
+            this.btnKasaİslemleri.Size = new System.Drawing.Size(101, 102);
+            this.btnKasaİslemleri.TabIndex = 0;
+            this.btnKasaİslemleri.UseVisualStyleBackColor = false;
+            // 
+            // btnMusteriler
+            // 
+            this.btnMusteriler.BackColor = System.Drawing.Color.Transparent;
+            this.btnMusteriler.BackgroundImage = global::Cielo.Properties.Resources.Musteri;
+            this.btnMusteriler.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnMusteriler.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMusteriler.Location = new System.Drawing.Point(364, 72);
+            this.btnMusteriler.Name = "btnMusteriler";
+            this.btnMusteriler.Size = new System.Drawing.Size(169, 87);
+            this.btnMusteriler.TabIndex = 0;
+            this.btnMusteriler.UseVisualStyleBackColor = false;
+            // 
+            // btnRezervasyon
+            // 
+            this.btnRezervasyon.BackColor = System.Drawing.Color.Transparent;
+            this.btnRezervasyon.BackgroundImage = global::Cielo.Properties.Resources.REZz;
+            this.btnRezervasyon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnRezervasyon.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRezervasyon.Location = new System.Drawing.Point(150, 165);
+            this.btnRezervasyon.Name = "btnRezervasyon";
+            this.btnRezervasyon.Size = new System.Drawing.Size(101, 102);
+            this.btnRezervasyon.TabIndex = 0;
+            this.btnRezervasyon.UseVisualStyleBackColor = false;
+            // 
+            // btnMasaİslemleri
+            // 
+            this.btnMasaİslemleri.BackColor = System.Drawing.Color.Transparent;
+            this.btnMasaİslemleri.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnMasaİslemleri.BackgroundImage")));
+            this.btnMasaİslemleri.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnMasaİslemleri.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMasaİslemleri.Location = new System.Drawing.Point(150, 72);
+            this.btnMasaİslemleri.Name = "btnMasaİslemleri";
+            this.btnMasaİslemleri.Size = new System.Drawing.Size(208, 87);
+            this.btnMasaİslemleri.TabIndex = 0;
+            this.btnMasaİslemleri.UseVisualStyleBackColor = false;
+            // 
+            // btnMutfak
+            // 
+            this.btnMutfak.BackColor = System.Drawing.Color.Transparent;
+            this.btnMutfak.BackgroundImage = global::Cielo.Properties.Resources.mutfak;
+            this.btnMutfak.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnMutfak.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMutfak.Location = new System.Drawing.Point(364, 165);
+            this.btnMutfak.Name = "btnMutfak";
+            this.btnMutfak.Size = new System.Drawing.Size(169, 102);
+            this.btnMutfak.TabIndex = 0;
+            this.btnMutfak.UseVisualStyleBackColor = false;
+            // 
+            // FrmMenu
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(690, 434);
+            this.Controls.Add(this.btnRaporlar);
+            this.Controls.Add(this.btnCıkıs);
+            this.Controls.Add(this.btnRapor);
+            this.Controls.Add(this.btnKasaİslemleri);
+            this.Controls.Add(this.btnMusteriler);
+            this.Controls.Add(this.btnRezervasyon);
+            this.Controls.Add(this.btnMasaİslemleri);
+            this.Controls.Add(this.btnMutfak);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmMenu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmMenu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMutfak;
+        private System.Windows.Forms.Button btnRezervasyon;
+        private System.Windows.Forms.Button btnMusteriler;
+        private System.Windows.Forms.Button btnKasaİslemleri;
+        private System.Windows.Forms.Button btnMasaİslemleri;
+        private System.Windows.Forms.Button btnRapor;
+        private System.Windows.Forms.Button btnRaporlar;
+        private System.Windows.Forms.Button btnCıkıs;
+    }
+}
